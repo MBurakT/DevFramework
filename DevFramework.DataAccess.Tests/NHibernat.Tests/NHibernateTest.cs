e33977879d1732f84332c88dca85c6bd941df2e6
MBurakT <mbtbrk11@outlook.com>
2022-04-24 13:14:49 +0300

    BTK 49.1

---
+++ DevFramework.DataAccess.Tests/NHibernat.Tests/NHibernateTest.cs
@@ -0,0 +1,31 @@
+﻿using DevFramework.Northwind.DataAccess.Concrete.NHibernate;
+using DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DevFramework.DataAccess.Tests.NHibernat.Tests
+{
+    [TestClass]
+    public class NHibernateTest
+    {
+        [TestMethod]
+        public void Get_all_returns_all_products()
+        {
+            NhProductDal nhProductDal = new NhProductDal(new SqlServerHelper());
+
+            var result = nhProductDal.GetList();
+
+            Assert.AreEqual(77, result.Count);
+        }
+
+        [TestMethod]
+        public void Get_all_with_paremeter_returns_filtered_products()
+        {
+            NhProductDal nhProductDal = new NhProductDal(new SqlServerHelper());
+
+            var result = nhProductDal.GetList(p => p.ProductName.Contains("ab"));
+
+            Assert.AreEqual(4, result.Count);
+        }
+    }
+}
---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/Mappings/CategoryMap.cs
@@ -0,0 +1,22 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class CategoryMap : EntityTypeConfiguration<Category>
+    {
+        public CategoryMap()
+        {
+            ToTable(@"Categories", @"dbo");
+            HasKey(c => c.CategoryId);
+
+            Property(c => c.CategoryId).HasColumnName("CategoryID");
+            Property(c => c.CategoryName).HasColumnName("CategoryName");
+        }
+    }
+}
